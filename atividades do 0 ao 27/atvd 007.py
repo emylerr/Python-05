7b1ffd75c30f5116f8abdd8cd129f2cefca75d81
Gabriely Ribeiro <emylegabriely371@gmail.com>
2026-05-12 13:27:13 -0300

    Python

--- atividades do 0 ao 27/atvd 007.py
+++ atividades do 0 ao 27/atvd 007.py
@@ -2,26 +2,30 @@
 print('-' * 70)
 valor = []
 maior = menor = 0
-posmax = posmin = 0
 
 for cont in range(0, 5):
     valor.append(int(input(f'Digite um valor na posição {cont}: ')))
 
     if cont == 0:
         maior = menor = valor[cont]
-        posmax = posmin = cont
     else:
         if valor[cont] > maior:
             maior = valor[cont]
-            posmax = cont
 
         if valor[cont] < menor:
             menor = valor[cont]
-            posmin = cont
 print('-' * 70)
 print(f'Você digitou os valores: {valor}')
-print(f'O maior número encontrado foi {maior} na posição {posmax}')
-print(f'O menor valor encontrado foi {menor} na posição {posmin}')
+print(f'O maior número encontrado foi {maior} na(s) posição(ões): ', end ='')
+for ind, val in enumerate(valor):
+    if val == maior:
+        print(f'{ind} ', end='')
+print()
+print(f'O menor número encontrado foi {menor} na(s) posição(ões): ', end ='')
+for ind, val in enumerate(valor):
+    if val == menor:
+        print(f'{ind} ', end='')
+print()
 print('-' * 70)
 
 #com o index, min e max
