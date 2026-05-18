1e5edd75e92009d527e6f32eb7e30d9845a9e21d
Gabriely Ribeiro <emylegabriely371@gmail.com>
2026-05-18 20:52:15 -0300

    Python

--- atividades do 0 ao 27/atvd 009.py
+++ atividades do 0 ao 27/atvd 009.py
@@ -1,3 +1,17 @@
 #Valores nas posições que foram digitadas
 print('-' * 70)
 lista = []
+for cont in range(0,5):
+    num = int(input('Digite um número: '))
+    if cont == 0 or num > lista[-1]:
+        lista.append(num)
+    else:
+        pos = 0
+        while pos < len(lista):
+            if num <= lista[pos]:
+                lista.insert(pos, num)
+                break
+            pos += 1
+print()
+print(f'Os valores em ordem são: {lista}')
+print('-' * 70)
