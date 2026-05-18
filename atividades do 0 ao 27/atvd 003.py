1b490f7a4abd6f25d47d768441da2f6e2ff8b05c
Gabriely Ribeiro <emylegabriely371@gmail.com>
2026-05-18 20:58:23 -0300

    Python

--- atividades do 0 ao 27/atvd 003.py
+++ atividades do 0 ao 27/atvd 003.py
@@ -8,5 +8,5 @@ print(f'Os números sorteados foram: ', end='')
 for n in num:
     print(f'{n}', end='')
 print(f'\nO maior valor sorteado foi: {max(num)}')
-print(f'O mnenor valor sorteado foi: {min(num)}')
+print(f'O menor valor sorteado foi: {min(num)}')
 print('-' * 70)
