40612e809d7d160ed53dea4d66f57f32f3749a46
Gabriely Ribeiro <emylegabriely371@gmail.com>
2026-05-18 20:56:52 -0300

    Python

--- atividades do 0 ao 27/atvd 009.py
+++ atividades do 0 ao 27/atvd 009.py
@@ -5,11 +5,13 @@ for cont in range(0,5):
     num = int(input('Digite um número: '))
     if cont == 0 or num > lista[-1]:
         lista.append(num)
+        print(f'Valor adicionado no final da lista.')
     else:
         pos = 0
         while pos < len(lista):
             if num <= lista[pos]:
                 lista.insert(pos, num)
+                print(f'Adicionado na posição: {pos}')
                 break
             pos += 1
 print()
