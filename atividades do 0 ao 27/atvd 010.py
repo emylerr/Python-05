e375428dc4f6da4f15350c74b47e4f35b6513374
Gabriely Ribeiro <emylegabriely371@gmail.com>
2026-05-18 21:17:37 -0300

    Python

--- atividades do 0 ao 27/atvd 010.py
+++ atividades do 0 ao 27/atvd 010.py
@@ -1,1 +1,19 @@
 #Extraindo dados de uma lista.
+print('-' * 70)
+lista = []
+while True:
+    lista.append(int(input('Digite um número: ')))
+    resp = str(input('Você quer digitar +1 valor? [S/N]: ')).strip().upper()[0]
+    if resp in 'N':
+        break
+print()
+print(f'Você digitou {len(lista)} elementos')
+
+lista.sort(reverse = True)
+print(f'Os valores em ordem decrescente são: {lista}')
+
+if 5 in lista:
+    print(f'O número 5 faz parte da lista.')
+else:
+    print(f'O número 5 não foi digitado.')
+print('-' * 70)
